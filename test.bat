22845ffcb2350d569b3266cbc4b217f6352fbe22
sheetalsakore <59859655+sheetalsakore@users.noreply.github.com>
2020-01-24 16:38:54 +0530

    Delete test.bat

--- test.bat
+++
@@ -1,7 +0,0 @@
-echo off 
-cd\
-cmd.exe if not exist C:\ATP mkdir C:\ATP
-cmd.exe if not exist C:\ATP\Client mkdir C:\ATP\Client
-cmd.exe if not exist C:\ATP\Client mkdir C:\ATP\ClientExtra
-pushd C:\ATP
-popd
